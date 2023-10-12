2c5e1affd0dc205e08ef04784b57755422693cd1
Carol Wargo <Carolwargo@gmail.com>
2023-10-12 15:06:06 -0400

    update

---
+++ schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstname: String
+    lastname: String
+    cellphone: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    addUser(firstname: String!,lastname:String!, cellphone: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+`;
+
+module.exports = typeDefs;
