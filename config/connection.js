44c319524532bfaedc29511d2157b147bf45f1d2
Carol Wargo <Carolwargo@gmail.com>
2023-10-12 18:25:22 -0400

    update connction to mongo db

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI; 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campsDB');
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+
 
 module.exports = mongoose.connection;
+
+
+
