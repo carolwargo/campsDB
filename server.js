173b35aa5331f5c73b23960a42a60bde1d46ea1c
Carol Wargo <Carolwargo@gmail.com>
2023-10-12 11:06:18 -0400

    UPDATE

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 const cors = require('cors');
+require('dotenv').config();
+
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
+
 
 // Add the auth middleware here
 app.use(cors());
 app.use((req, res, next) => authMiddleware({ req, res, next }));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your_database_name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// Use the routes defined in routes/index.js
-app.use(routes);
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
