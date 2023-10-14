e886aa6ca1a88e4d631e82a05011449d3b34ec77
Carol Wargo <Carolwargo@gmail.com>
2023-10-14 16:36:12 -0400

    added auth mutation and queries. added to app.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,42 @@
 import { gql } from '@apollo/client';
 
 export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!) {
-    addProfile(name: $name) {
+  mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_PLAYER = gql`
+  mutation addPlayer($profileId: ID!, $player: String!) {
+    addPlayer(profileId: $profileId, player: $player) {
+      _id
+      name
+      players
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const REMOVE_PLAYER = gql`
+  mutation removePlayer($player: String!) {
+    removePlayer(player: $player) {
       _id
       name
       players
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,23 @@ export const QUERY_PROFILES = gql`
     }
   }
 `;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      players
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      name
+     players
+    }
+  }
+`;
