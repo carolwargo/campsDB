80e8d9c3ba7b74103585fefb273c39ac75e4bff3
Carol Wargo <Carolwargo@gmail.com>
2023-10-20 09:51:36 -0400

    added new images

---
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import John from '../../assets/images/John.png'; // Make sure to import your image
+import Guy from '../../assets/images/Guy.png'; // Make sure to import your image
+import Joe from '../../assets/images/Joe.png'; // Make sure to import your image
+import Ryan from '../../assets/images/Ryan.png'; // Make sure to import your image
+
+const cardData = [
+  {
+    id: 1,
+  
+    image: Guy,
+  },
+  {
+    id: 2,
+  
+    image: John,
+  },
+  {
+    id: 3,
+  
+    image: Ryan,
+  },
+  {
+    id: 4,
+  
+    image: Joe,
+  },
+];
+
+export default function GridExample() {
+  return (
+    <Row xs={1} md={2} lg={4} className="g-4">
+      {cardData.map((card) => (
+        <Col key={card.id}>
+          <Card>
+            <Card.Img variant="top" src={card.image} />
+            <Card.Body>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Text>{card.text}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
--- client/src/views/Homepage/Homepage.js
+++ client/src/views/Homepage/Homepage.js
@@ -1,55 +1,34 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import ProfileList from '../../components/ProfileList';
+import { QUERY_PROFILES } from '../../utils/queries';
+import Cover from "../../assets/images/Cover.png"; // Import your background cover image
 
-import "../Homepage/homepage.css";
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_PROFILES);
+  const profiles = data?.profiles || [];
 
-
-export default function Home() {
   return (
-    <>
-      {/* HEADER START */}
-      <div className="container-body">
-        <div className="main-col">
-          <div className="content-container">
-            <div className="content">
-              {/*<h2>WELCOME TO</h2>*/}
-              <h1>301 CATCHING</h1>
-              <h4 style={{ fontFamily: "serif" }}>
-                {" "}
-                The <span className="red-text">
-                  {" "}
-                  "FOUNDATIONAL EXPERTS"
-                </span>{" "}
-                for everything catching:
-                <br></br>
-                Providing Players with the skills to master their craft &
-                elevate their game.
-              </h4>
-
-              <h4 className="pt-3 mt-4 pb-2 text-decoration-underline">
-                UPCOMING <span>Events</span>
-              </h4>
-              <h5 className="pb-3">
-                Explore upcoming sessions and events with 301_CATCHING!
-                <span style={{ color: "red" }}>
-                  <br></br>Click "LEARN MORE"
-                </span>{" "}
-                to see Available Sessions & Events.
-              </h5>
-              <div className="btn-container" style={{ display: 'flex' }}>
-  <NavLink to="/login" className="login-btn">
-    LEARN MORE
-  </NavLink>
-  <NavLink to="/signup" className="signup-btn">
-    LEARN MORE
-  </NavLink>
-</div>
-
-            </div>
-          </div>
+    <main style={{ 
+      backgroundImage: `url(${Cover})`, // Set background image
+      backgroundSize: 'cover', // Make sure it covers the entire container
+      backgroundPosition: 'center center', // Center the background image
+      opacity: '0.8', // Set opacity (adjust as needed)
+    }}>
+      <div className="flex-row justify-center">
+        <div className="col-12 col-md-10 my-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ProfileList
+              profiles={profiles}
+              title="Here's the current roster of friends..."
+            />
+          )}
         </div>
       </div>
-    </>
+    </main>
   );
-}
+};
 
+export default Home;
