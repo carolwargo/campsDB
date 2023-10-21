727090fc5f4341c3d2c6479ab6d96a3c7f6a1f89
Carol Wargo <Carolwargo@gmail.com>
2023-10-21 17:34:12 -0400

    removed User models

--- client/src/views/Homepage/Homepage.js
+++ client/src/views/Homepage/Homepage.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import ProfileList from '../../components/ProfileList';
 import { QUERY_PROFILES } from '../../utils/queries';
-import Cover from "../../assets/images/Cover.png"; // Import your background cover image
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
 
   return (
-    <main style={{ 
-      backgroundImage: `url(${Cover})`, // Set background image
-      backgroundSize: 'cover', // Make sure it covers the entire container
-      backgroundPosition: 'center center', // Center the background image
-      opacity: '0.8', // Set opacity (adjust as needed)
-    }}>
+    <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
           {loading ? (
